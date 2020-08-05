041b65e1d64299e3079805fe904b2f22b4ad9b2e
tengzhang123 <tengzhang156@126.com>
2020-08-05 17:22:50 -0500

    Update Get_express_data.R

--- R/Get_express_data.R
+++ R/Get_express_data.R
@@ -5,5 +5,12 @@ Get_express_data <- function(INPUT_BAM, TREATED_INPUT_BAM=character(0),annot_fil
   counts_data <- gene_count$counts
   countMatrix <- sapply(as.matrix(counts_data), as.numeric)
   gene_countmatrix <- matrix(countMatrix, nrow=nrow(counts_data), ncol = ncol(counts_data))
-  return(gene_countmatrix)
+  gene_ID <- as.character(rownames(countmatrixgene)) 
+  tans_name <- select(org.Hs.eg.db, keys=gene_ID, columns = c("SYMBOL"),keytype="ENTREZID")
+  gene_name <- as.character(tans_name[,2]) 
+  gene_countsdata <- cbind(gene_name, countmatrixgene)
+  gene_countdata <- as.data.frame(gene_countsdata)
+  gene_countdata <- na.omit(gene_countdata)
+  rownames(gene_countdata)<-NULL
+  return(gene_countdata)
 }
